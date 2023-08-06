64e1ba7205a2aa949f14fcff0afc6694656378d8
Kidd254 <lorenkioko@gmail.com>
2023-08-06 18:45:33 +0300

    Add routing to the respecting pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Home from './views/Home';
+import Projects from './views/Projects';
+import Contacts from './views/Contacts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='home' element={<Home />}/>
+        <Route path='projects' element={<Projects />}/>
+        <Route path='contacts' element={<Contacts />}/>
+    </Routes>
   );
 }
 
