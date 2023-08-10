fb04d3075c37f4464e540fef4189f5fb38160f62
Kidd254 <lorenkioko@gmail.com>
2023-08-10 17:46:49 +0300

    Add styling to home page

--- src/componentss/HomeContent.jsx
+++ src/componentss/HomeContent.jsx
@@ -6,11 +6,11 @@ import styles from '../styles/HomeContent.module.css';
 const HomeContent = () => (
   <div className={`container-fluid d-flex flex-column justify-content-center align-items-center ${styles.background}`}>
 
-    <div className="col-md-6 mt-5 pt-5">
-      <h1>I&apos;m Lawrence Glad to See You!</h1>
+    <div className={`col-md-5 mt-5 pt-5 transition ${styles.space}`}>
+      <h1>I&apos;m Lawrence                   Glad to See                     You!</h1>
     </div>
 
-    <div className=" col-md-6">
+    <div className=" col-md-5">
       <p>
         I&apos;m a software developer! I can help you build a product ,
         feature or website Look through some of my work and experience!
@@ -18,15 +18,15 @@ const HomeContent = () => (
       </p>
     </div>
 
-    <div className="col-md-6">
+    <div className="col-md-5 pb-2">
       <h2>Lets Connect</h2>
     </div>
-    <div className="col-md-6">
-      <a href="https://github.com/Kidd254" aria-label="Github" className="p-2"><FaGithub size={32} /></a>
-      <a href="https://www.linkedin.com/in/lawrence-muema-kioko" aria-label="Linkedin" className="p-2"><FaLinkedin size={32} /></a>
-      <a href="https://medium.com/@lorenkioko" aria-label="Medium" className="p-2"><FaMedium size={32} /></a>
-      <a href="https://twitter.com/lawrenc98789206" aria-label="Twitter" className="p-2"><FaTwitter size={32} /></a>
-      <a href="https://wellfound.com/u/lawrence-muema-kioko" aria-label="Angellist" className="p-2"><FaAngellist size={32} /></a>
+    <div className="col-md-5">
+      <a href="https://github.com/Kidd254" aria-label="Github" className={`pt-2 pr-2 pb-2 ${styles.github}`}><FaGithub size={32} /></a>
+      <a href="https://www.linkedin.com/in/lawrence-muema-kioko" aria-label="Linkedin" className={`p-2 ${styles.linkedin}`}><FaLinkedin size={32} /></a>
+      <a href="https://medium.com/@lorenkioko" aria-label="Medium" className={`p-2 ${styles.medium}`}><FaMedium size={32} /></a>
+      <a href="https://twitter.com/lawrenc98789206" aria-label="Twitter" className={`p-2 ${styles.twitter}`}><FaTwitter size={32} /></a>
+      <a href="https://wellfound.com/u/lawrence-muema-kioko" aria-label="Angellist" className={`p-2 ${styles.angelist}`}><FaAngellist size={32} /></a>
     </div>
 
   </div>
