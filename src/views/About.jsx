fc8d2953a672e582ce1e12eb36e0e613c519e2f8
Kidd254 <lorenkioko@gmail.com>
2023-08-11 23:28:48 +0300

    Add about myself page

---
+++ src/views/About.jsx
@@ -0,0 +1,13 @@
+import Layout from './Layout';
+import AboutContent from '../componentss/AboutContent';
+
+const About = () => (
+  <>
+    <Layout />
+    <section>
+      <AboutContent />
+    </section>
+  </>
+);
+
+export default About;
