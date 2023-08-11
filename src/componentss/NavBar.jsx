a85aa006f4719175d83ef832cb7abcd0ab0be460
Kidd254 <lorenkioko@gmail.com>
2023-08-11 23:29:23 +0300

    Add a link to about myself page

--- src/componentss/NavBar.jsx
+++ src/componentss/NavBar.jsx
@@ -3,10 +3,11 @@ import MenuLink from './MenuLink';
 const NavBar = () => (
   <header>
     <div className="container-fluid">
-      <nav className="navbar navbar-expand-md bg-primary">
+      <nav className="navbar navbar-expand-md bg-white">
         <ul className="navbar-nav list-unstyled">
           <MenuLink url="/home" pageName="Home" />
           <MenuLink url="/projects" pageName="My Projects" />
+          <MenuLink url="/about" pageName="About Myself" />
           <MenuLink url="/contacts" pageName="Contact Me" />
         </ul>
       </nav>
